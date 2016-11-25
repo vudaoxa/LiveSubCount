69c0837f5a81cb5bd64dfab73d027775c1e6e351
vunq111 <vunq@i-com.vn>
2016-11-25 18:03:48 +0700

    sfs

---
+++ LC/app/src/main/java/net/lc/LCApplication.kt
@@ -0,0 +1,10 @@
+package net.lc
+
+import android.app.Application
+
+/**
+ * Created by MRVU on 11/25/2016.
+ */
+class LCApplication : Application{
+
+}
---
+++ LC/app/src/main/java/net/lc/utils/NetConstants.kt
@@ -0,0 +1,11 @@
+package net.lc.utils
+
+/**
+ * Created by MRVU on 11/25/2016.
+ */
+
+object NetConstants {
+    val RESPONSE_NULL = "null"
+    var BASE_URL = "https://www.googleapis.com/youtube/v3/channels"
+    val API_LOGIN = "event"
+}
