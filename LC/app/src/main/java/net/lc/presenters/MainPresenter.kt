214b08342a28e6b5e6125587d8d72a07db115d7a
vunq111 <vunq@i-com.vn>
2016-12-08 01:18:10 +0700

    fdfd

--- LC/app/src/main/java/net/lc/presenters/MainPresenter.kt
+++ LC/app/src/main/java/net/lc/presenters/MainPresenter.kt
@@ -1,5 +1,6 @@
 package net.lc.presenters
 
+import android.util.Log
 import com.tieudieu.util.DebugLog
 import net.lc.utils.Constants
 import net.lc.utils.Models
@@ -10,6 +11,12 @@ import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import rx.internal.operators.OperatorReplay.observeOn
+import javax.xml.datatype.DatatypeConstants.SECONDS
+import rx.android.schedulers.AndroidSchedulers.mainThread
+import rx.android.schedulers.AndroidSchedulers
+
+
 
 /**
  * Created by HP on 11/28/2016.
@@ -19,14 +26,26 @@ object MainPresenter {
     fun periodicRequestChannelInfo(channelName: String){
         val scheduler = Schedulers.from(Executors.newSingleThreadExecutor())
         val obs = Observable.interval(5, TimeUnit.SECONDS, scheduler)
-                .flatMap { tick -> requestChannelInfo() }
-                .
-
+                .flatMap { tick ->  requestChannelInfo()}
+                .subsc
+//        Observable.interval(30, TimeUnit.SECONDS, scheduler)
+//                .flatMap { tick -> service.getAthletes(1, 0L) }
+//                // Performed on service.getAthletes() observable
+//                .subscribeOn(scheduler)
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .doOnError({ err -> Log.d("APP", "Error retrieving athletes: " + err.toString()) })
+//                .retry()
+//                .flatMap(???({ Observable.from() }))
+//        .filter { athlete ->
+//            // Process Athlete here in the filter
+//            // Return true to always send messages but could filter them out too
+//            true
+//        }
     }
-    fun requestChannelInfo() {
-        if (isRequest)
-            return
-        isRequest = true
+    fun requestChannelInfo() :Models.ChannelListResponse{
+//        if (isRequest)
+//            return
+//        isRequest = true
         val xx = RetrofitService.instance.service.getChannelInfo(Constants.API_KEY, Constants.PART_STATISTICS, "SkyDoesMinecraft")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
