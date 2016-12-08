905625242ac92823d89624fcf28d3f4239cf1ad0
vunq111 <vunq@i-com.vn>
2016-12-08 07:32:59 +0700

    kjkj

--- LC/app/src/main/java/net/lc/utils/network/RetrofitService.kt
+++ LC/app/src/main/java/net/lc/utils/network/RetrofitService.kt
@@ -1,28 +1,16 @@
 package net.lc.utils.network
 
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.tieudieu.util.DebugLog
 import net.lc.utils.Constants
-
 import net.lc.utils.Models
 import net.lc.utils.NetConstants
-
-import java.io.IOException
-import java.util.concurrent.TimeUnit
-
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import okhttp3.ResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import rx.Observable
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by HP on 11/26/2016.
@@ -30,7 +18,6 @@ import rx.schedulers.Schedulers
 
 class RetrofitService {
     private val service: ApiServices
-//    private val isRefreshToken = false
 
     init {
         val interceptor = HttpLoggingInterceptor()
@@ -62,16 +49,6 @@ class RetrofitService {
     }
 
     fun getChannelInfo(channelName:String): Observable<Models.ChannelListResponse> {
-//        fun getChannelInfo(@Query("key") developerKey: String, @Query("part") id: String,
-//                           @Query("forUsername") forUsername: String): Observable<ChannelListResponse>
-//        service.getChannelInfo(Constants.API_KEY, Constants.PART_STATISTICS, channelName)
-//                .subscribeOn(Schedulers.newThread())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe{
-//                    artifact ->
-//                    println("${artifact.etag} (${artifact.items!!.get(0).statistics!!.viewCount})")
-//                }
-
         return Observable.create {
             subscriber ->
             val callResponse = service.getChannelInfo(Constants.API_KEY, Constants.PART_STATISTICS, channelName)
@@ -80,16 +57,6 @@ class RetrofitService {
             if (response.isSuccessful) {
                 val dataResponse = response.body()
                 DebugLog.e("dataResponse-------------- "+dataResponse)
-//                val news = dataResponse.children.map {
-//                    val item = it.data
-//                    RedditNewsItem(item.author, item.title, item.num_comments,
-//                            item.created, item.thumbnail, item.url)
-//                }
-//                val redditNews = RedditNews(
-//                        dataResponse.after ?: "",
-//                        dataResponse.before ?: "",
-//                        news)
-
                 subscriber.onNext(dataResponse)
                 subscriber.onCompleted()
             } else {
